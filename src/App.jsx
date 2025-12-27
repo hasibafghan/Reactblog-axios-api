7087cf739562980e1a6d5a75ce21e07398adf5cd
Hasib Alamyar <hasibalamyar2@gmail.com>
2025-12-28 00:05:17 +0430

    db.json is created for fake api record

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import { FaBeer } from 'react-icons/fa';
 
 function App() {
 
   return (
     <>
-     <h1>hello world</h1>
+     <h1><FaBeer /></h1>
+     <div><img src="https://next1code.ir/wp-content/uploads/2024/01/nodejs-course-cover.jpghttps://next1code.ir/wp-content/uploads/2024/01/tailwind2-course-cover-500x286.jpg" alt="" /></div>
+     <div>
+      <img src="" alt="" />
+     </div>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './main.css'
-import App from './App.jsx'
+import "bootstrap/dist/css/bootstrap.rtl.min.css";
+import "./main.css";
+import App from "./App.jsx";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ThemeProvider } from "react-bootstrap";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider dir="rtl">
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
